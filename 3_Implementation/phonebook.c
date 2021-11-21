7091af3815ddc06affd009187b648c2cad14c067
harshtheraj <81011974+harshtheraj@users.noreply.github.com>
2021-11-21 23:44:47 +0530

    Add files via upload

---
+++ 3_Implementation/phonebook.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<windows.h>
+
+void add_contact();
+void search_contact();
+void delete_contact();
+void view_all_contact();
---
+++ 3_Implementation/phonebook.h
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<windows.h>
+
+void main();
+void add_contact();
+void search_contact();
+void delete_contact();
+void view_all_contact();
